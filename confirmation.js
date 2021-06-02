db1b9f9aed31ee34656c6dc09823df1361edb3b6
Lareb Khan <74281376+larebskhan@users.noreply.github.com>
2021-06-02 11:05:20 -0400

    Update confirmation.js

--- confirmation.js
+++ confirmation.js
@@ -1,7 +1,7 @@
 var number = 0
 $(document).ready(function(){
     number = 1 + Math.floor(Math.random() * 999999999);
-    //alert(number);
+    alert(number);
     document.getElementById('confirmationNum').value = number;
     localStorage.setItem('ConfirmationNumber', number);
     //alert(localStorage.getItem("ConfirmationNumber"));
