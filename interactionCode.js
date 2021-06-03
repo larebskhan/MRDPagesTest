c595ff59977c70a2a41e08b40b6a1371d36d8d2b
David Keen <77408062+ddkeen@users.noreply.github.com>
2021-06-03 11:32:47 -0400

    projectEndDate cannot be in the past

--- interactionCode.js
+++ interactionCode.js
@@ -201,12 +201,24 @@ function validatetextboxes()
     } 
 }
 
-/*
+
+
 function minDate() 
 {
-    projectEndDate.min = new Date().toISOString().split("T")[0];
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
+    today = yyyy+'-'+mm+'-'+dd;
+    document.getElementById("projectEndDate").setAttribute("min", today);
 }
-*/
+
 
 function addInputBox(id, name)
 {
