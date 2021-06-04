d4d1ac2d05d14cb6a8f920e1e71d6ccd35e4a120
CJ5467 <cjseese23@gmail.com>
2021-06-04 12:15:59 -0400

    main

--- interactionCode.js
+++ interactionCode.js
@@ -34,6 +34,12 @@ function alertingInput(choice, id)
     }
 }
 
+function reviewMRD(){
+    validatetextboxes();
+    $('#submitBtn').click(function(){
+         $('#ticketTeamPrev').text($('#ticketTeam_1').val());
+    });
+}
 function saveJSON(text, filename)
 {
     //create anchor element
@@ -58,7 +64,6 @@ function serverWarning(id){
         document.getElementById('serverWarningText').innerHTML = "";
         //document.getElementById(id).style.borderColor = color;
 
-
     }
     else
     {
@@ -66,35 +71,8 @@ function serverWarning(id){
         //document.getElementById(id).style.borderColor = "orange";
         document.getElementById('serverWarningText').innerHTML = "<i class='fa fa-exclamation-circle' aria-hidden='true'></i><em style='font-size: 12px;'> Server ID does not start with a03 or a70!<em>";
     }
-    /*while(forEach(document.getElementById('addServer_1').onclick==true)){
-        idNum = id.match(/\d+/);
-        var str2 = document.getElementById('server'+idNum).value;
-        if(str2.startsWith("a03") || (str2.startsWith("a70"))){
-            document.getElementById('serverWarning'+idNum).className = "";
-            document.getElementById('serverWarningText'+idNum).innerHTML = "";
-        }
-        else{
-            document.getElementById('serverWarning'+idNum).className = "has-warning";
-            document.getElementById('serverWarningText'+idNum).innerHTML = "Warning! ServerID does not start with a03 or a70!";
-            //$("#serverWarningText").css("color", "orange");
-        }
-    }*/
    
     
-}
-
-function removeReq(id, name)
-{
-    idNum = id.match(/\d+/);
-    if(idNum > 1)
-    {
-        var requirement = name+idNum;
-        $('#'+requirement).empty();
-    }
-    if(idNum == 1)
-    {
-        alert('You cannot remove the first requirement');
-    }
 }
 
 //function that makes sure the project end date cannot be a date in the past
